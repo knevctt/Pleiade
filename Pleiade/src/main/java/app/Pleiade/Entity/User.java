32b4d78f31a4387de759ab18d23496dffa0fb53b
knevctt <knevctt@outlook.com>
2024-09-18 19:36:22 -0300

    End of entity User

--- Pleiade/src/main/java/app/Pleiade/Entity/User.java
+++ Pleiade/src/main/java/app/Pleiade/Entity/User.java
@@ -5,10 +5,14 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.time.LocalDate;
 
 @Entity
 @AllArgsConstructor
@@ -23,9 +27,23 @@ public class User {
     @NotBlank
     private String name;
 
+    private String lastName;
+
+    @NotBlank
+    private String Username;
+
     @NotBlank
     private String email;
 
     @NotBlank
     private String password;
+
+    @NotNull
+    private LocalDate dateOfBirth;
+
+    @NotBlank
+    private String institutionOrAffiliation;
+
+    @NotBlank
+    private String country;
 }
