595af9392442977ed0c6ba4752356ba2b3e155b7
knevctt <knevctt@outlook.com>
2024-09-17 21:01:31 -0300

    Initial commit

---
+++ Pleiade/src/main/java/app/Pleiade/PleiadeApplication.java
@@ -0,0 +1,13 @@
+package app.Pleiade;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PleiadeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PleiadeApplication.class, args);
+	}
+
+}
---
+++ Pleiade/src/test/java/app/Pleiade/PleiadeApplicationTests.java
@@ -0,0 +1,13 @@
+package app.Pleiade;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PleiadeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
