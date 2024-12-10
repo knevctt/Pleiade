6d6ebb2a17f3a0809ebf7426eda597d77c7dc9ec
knevctt <knevctt@outlook.com>
2024-12-10 20:10:56 -0300

    removido o que nao é mais usado

--- Pleiade/src/main/java/app/Pleiade/Repository/PessoaRepository.java
+++
@@ -1,9 +0,0 @@
-package app.Pleiade.Repository;
-
-import app.Pleiade.Entity.PessoasEntity;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PessoaRepository extends JpaRepository<PessoasEntity, Long> {
-
-    PessoasEntity findPessoaByDocumento(String documento);
-}
